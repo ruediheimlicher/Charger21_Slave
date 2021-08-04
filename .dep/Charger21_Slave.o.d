33424b1173fa6bc01885c4e63622f5b324666ca8
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2021-08-04 10:23:14 +0200

    add2

---
+++ .dep/Charger21_Slave.o.d
@@ -0,0 +1,16 @@
+Charger21_Slave.o: Charger21_Slave.c lcd.c lcd.h adc.c adc.h version.c \
+ usb_rawhid.c usb_rawhid.h
+
+lcd.c:
+
+lcd.h:
+
+adc.c:
+
+adc.h:
+
+version.c:
+
+usb_rawhid.c:
+
+usb_rawhid.h:
